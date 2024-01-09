42329168542a7e8f05ef67daadd2805a0c4837cd
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-09 12:36:07 -0700

    npm passing tests

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+
+  function handleAddCart() {
+    setIsInCart(!isInCart)
+  }
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddCart} className={isInCart ? "add" : "remove"}>
+        {isInCart ? "Remove from cart" : "Add to cart"} 
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
+
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleChange(event) {
+    setSelectedCategory(event.target.value)
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    return selectedCategory === "All" ? true : item.category === selectedCategory
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +34,21 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
+
+
+
+
+// const [foods, setFoods] = useState(spicyFoods);
+// const [filterBy, setFilterBy] = useState("All");
+
+// const foodsToDisplay = foods.filter((food) => {
+//   if (filterBy === "All") {
+//     return true;
+//   } else {
+//     return food.cuisine === filterBy;
+//   }
+// });
+
+
