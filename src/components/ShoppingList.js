3102ef8977940c39ddc6faf98d7a7239362ae44e
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-12 14:02:03 -0700

    dark mode toggle attempted

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("")
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
